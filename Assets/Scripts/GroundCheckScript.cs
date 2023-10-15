6cd0276df777485c83010db3bd98e1964e6086ef
Brower161 <marioaparteche@gmail.com>
2023-10-15 18:33:31 +0200

    Initial commit

---
+++ Assets/Scripts/GroundCheckScript.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheckScript : MonoBehaviour
+{
+    public bool tocosuelo;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Suelo")
+        {
+            tocosuelo = true;
+            //Debug.Log("true");
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Suelo")
+        {
+            tocosuelo = false;
+            //Debug.Log("false");
+        }
+    }
+
+}
